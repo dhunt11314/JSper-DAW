720d7e3374d72ac8a6889d617fe3eb65f7320a0f
dhunt11314 <hunterdavidj11@gmail.com>
2025-06-22 17:27:09 +0100

    fixed search, improved deletion. V1.1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let selectedNotes = [];
 let allNotes = [];
 const noteHeight = 10;
 const lowPass = new Tone.AutoFilter("1000").toDestination();
-const reverb = new Tone.Reverb(1.5).toDestination();
+const reverb = new Tone.Reverb(2).toDestination();
 const synth = new Tone.PolySynth().connect(reverb).connect(lowPass);
 function setup() {
     createCanvas(windowWidth*2, 840);
@@ -55,6 +55,12 @@ function draw() {
         for (let j = 1; j < timeSig.top; j++) {
             line(i*pixelsPerBar+j*pixelsPerBeat, 0, i*pixelsPerBar+j*pixelsPerBeat, height);
         }
+        noStroke();
+        fill(40,40,40)
+        for (let i = 0; i < allNotes.length; i++) {
+            text(allNotes[i],5,height-(i*noteHeight));
+        }
+        strokeWeight(1)
     }
     if (instruments.length>0) {
         drawSequence(instruments[currentInstrument].notes);
@@ -63,8 +69,8 @@ function draw() {
     let pixels = timeToPixels(Tone.Transport.seconds)
     line(pixels,0,pixels,height);
     noStroke();
-    text("Transport time: "+Tone.Transport.seconds, 5,10);
-    text("Transport state: "+Tone.Transport.state, 5,20);
+    text("Transport time: "+Tone.Transport.seconds, 30,10);
+    text("Transport state: "+Tone.Transport.state, 30,20);
     stroke(0,0,255);
     let quantisedX = quantiseX(mouseX);
     line(quantisedX,0,quantisedX,height);
@@ -72,31 +78,36 @@ function draw() {
 function quantiseX(x) {
     let noteLock = document.getElementById("gridLock").value;
     let pixelsPerNoteLock = pixelsPerBeat*(timeSig.bottom/4)*noteLock;
-    return Math.floor(x/pixelsPerNoteLock)*pixelsPerNoteLock;
+    return {quantisedX:Math.floor(x/pixelsPerNoteLock)*pixelsPerNoteLock, noteLock};
 }
 function mousePressed() {
     if (!ready) {
         Tone.start();
         ready = true;
     }
-    let quantisedX = quantiseX(mouseX);
+    let quantisedX = quantiseX(mouseX).quantisedX;
     let time = pixelsToTime(quantisedX);
     let pixelsAboveBottomOfCanvas = height - mouseY
     let noteNum = Math.floor(pixelsAboveBottomOfCanvas/noteHeight);
     let pitch = allNotes[noteNum];
-    let note = findNoteAtTime(time);
+    let note = findNoteAtLocation(time,pitch);
+
+    let beats = (quantisedX/pixelsPerBeat)/2;
     if (!keyIsDown(SHIFT)) {
         selectedNotes = [];
     }
-    if (keyIsDown(CONTROL)) {
-        synth.triggerAttackRelease(pitch,"8n");
-        addNote(pitch, noteNum, time);
-    }
     if (note) {
-        let noteTop = height-(note.noteNum*noteHeight)-noteHeight;
-        let noteBottom = noteTop+noteHeight;
-        if (mouseY>=noteTop && mouseY <= noteBottom) {
-            selectedNotes.push(note);
+        selectedNotes.push(note);
+    }
+    if (keyIsDown(CONTROL)) {
+        if (note) {
+            //delete the note
+            instruments[currentInstrument].notes.splice(instruments[currentInstrument].notes.indexOf(note), 1);
+        }
+        else {
+            //add a note
+            synth.triggerAttackRelease(pitch, "8n");
+            addNote(pitch, noteNum, beats);
         }
     }
 }
@@ -139,19 +150,13 @@ function drawSequence(sequence) {
 function isTimeInNote(noteStart, noteDuration, timePoint) {
     return timePoint>=noteStart && timePoint<noteStart+noteDuration
 }
-function findNoteAtTime(time) {
-    console.log("searching at time "+time);
-    let total = 0;
+function findNoteAtLocation(time,pitch) {
     let sequence = instruments[currentInstrument].notes;
     for (let i=0; i<sequence.length; i++) {
-        console.log("loop");
-        let startTime = total
         let duration = Tone.Time(sequence[i].duration);
-        if (isTimeInNote(startTime, duration,time)) {
-            console.log("found note");
+        if (isTimeInNote(sequence[i].noteStart, duration,time) && sequence[i].note===pitch) {
             return sequence[i];
         }
-        total += Tone.Time(sequence[i].duration);
     }
 }
 function pixelsToTime(x) {
@@ -184,9 +189,9 @@ function setDuration(target) {
 function tempoChange(change) {
     Tone.Transport.bpm.value += change;
 }
-function addNote(pitch, noteNum, startTime) {
+function addNote(pitch, noteNum, startBeat) {
     synth.triggerAttackRelease(pitch,"8n")
-    instruments[currentInstrument].notes.push({isRest:false, note:pitch, duration:tempDuration, noteNum, noteStart:startTime});
+    instruments[currentInstrument].notes.push({isRest:false, note:pitch, duration:tempDuration, noteNum, noteStart:startBeat });
 }
 function addRest() {
     instruments[currentInstrument].notes.push({isRest:true, duration:tempDuration});
@@ -203,11 +208,9 @@ function playSequences() {
                 longestSequence = sequenceLength;
             }
         }
-        console.log(longestSequence);
         Tone.Transport.start();
-        console.log(longestSequence);
         Tone.Transport.schedule((time)=>{
-            Tone.Transport.stop(time);
+            Tone.Transport.stop(time+0.1);
         }, longestSequence);
     }
     else if (Tone.Transport.state === "paused") {
@@ -229,25 +232,13 @@ function playSequence(instrument) {
 function clearSequence() {
     instruments[currentInstrument].notes = [];
 }
-function deleteLastNote() {
-    instruments[currentInstrument].notes.pop();
-}
-let noteContainer = document.getElementById("notes");
 let notesList = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 let noteNum = 0;
 for (let octave = 1; octave < 8; octave++) {
     for (let note of notesList) {
-        let button = document.createElement("button");
-        button.textContent = note+octave;
         allNotes.push(note+octave);
-        button.className = "button";
-        let fooNoteNum = noteNum
-        button.onclick = function(){addNote(note+octave, fooNoteNum);}
-        noteContainer.appendChild(button);
         noteNum++
     }
-    let lineBreak = document.createElement("br");
-    noteContainer.appendChild(lineBreak);
 }
 function saveSequences() {
     let a = document.createElement("a");
@@ -258,7 +249,6 @@ function saveSequences() {
 }
 function loadFile() {
     let files = document.getElementById("loadFile").files;
-    console.log(files[0]);
     const reader = new FileReader();
     reader.onload = (e) => {
         let tempSequences = JSON.parse(atob(e.target.result.substring("data:application/json;base64,".length)));
